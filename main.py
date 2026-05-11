3d595b062212452fc185c2f8680d9d091f1616cf
Raptor-cyber <142733350+Raptor-cyber@users.noreply.github.com>
2026-05-11 15:50:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import uvicorn
 
 app = FastAPI(title="Megadriod Innovation Labs - Live Infrastructure Scanner")
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 SCAN_PORTS = [21, 22, 23, 25, 53, 80, 110, 111, 135, 139, 143, 443, 445, 993, 995, 1723, 3306, 3389, 5900, 8000, 8080, 8443]
